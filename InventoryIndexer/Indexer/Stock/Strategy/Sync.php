68850aa833faeed8899eec598867a18a9af4e83c
Jeanmarcos Juarez <janmarcoj@gmail.com>
2026-07-12 00:50:46 -0400

    perf(inventory-indexer): parallelize full stock reindex by stock dimension

--- InventoryIndexer/Indexer/Stock/Strategy/Sync.php
+++ InventoryIndexer/Indexer/Stock/Strategy/Sync.php
@@ -7,6 +7,7 @@
 namespace Magento\InventoryIndexer\Indexer\Stock\Strategy;
 
 use Magento\Framework\App\ResourceConnection;
+use Magento\Indexer\Model\ProcessManager;
 use Magento\InventoryCatalogApi\Api\DefaultStockProviderInterface;
 use Magento\InventoryIndexer\Indexer\InventoryIndexer;
 use Magento\InventoryIndexer\Indexer\Stock\GetAllStockIds;
@@ -70,6 +71,11 @@ class Sync
     private $prepareReservationsIndexData;
 
     /**
+     * @var ProcessManager
+     */
+    private $processManager;
+
+    /**
      * $indexStructure is reserved name for construct variable in index internal mechanism
      *
      * @param GetAllStockIds $getAllStockIds
@@ -81,6 +87,7 @@ class Sync
      * @param DefaultStockProviderInterface $defaultStockProvider
      * @param ReservationsIndexTable $reservationsIndexTable
      * @param PrepareReservationsIndexData $prepareReservationsIndexData
+     * @param ProcessManager $processManager
      */
     public function __construct(
         GetAllStockIds $getAllStockIds,
@@ -91,7 +98,8 @@ class Sync
         IndexTableSwitcherInterface $indexTableSwitcher,
         DefaultStockProviderInterface $defaultStockProvider,
         ReservationsIndexTable $reservationsIndexTable,
-        PrepareReservationsIndexData $prepareReservationsIndexData
+        PrepareReservationsIndexData $prepareReservationsIndexData,
+        ProcessManager $processManager
     ) {
         $this->getAllStockIds = $getAllStockIds;
         $this->indexStructure = $indexStructureHandler;
@@ -102,6 +110,7 @@ class Sync
         $this->defaultStockProvider = $defaultStockProvider;
         $this->reservationsIndexTable = $reservationsIndexTable;
         $this->prepareReservationsIndexData = $prepareReservationsIndexData;
+        $this->processManager = $processManager;
     }
 
     /**
@@ -134,42 +143,64 @@ class Sync
      */
     public function executeList(array $stockIds): void
     {
+        $stocksToReindex = [];
         foreach ($stockIds as $stockId) {
-            if ($this->defaultStockProvider->getId() === (int)$stockId) {
-                continue;
+            if ($this->defaultStockProvider->getId() !== (int)$stockId) {
+                $stocksToReindex[] = (int)$stockId;
             }
-
-            $replicaIndexName = $this->indexNameBuilder
-                ->setIndexId(InventoryIndexer::INDEXER_ID)
-                ->addDimension('stock_', (string)$stockId)
-                ->setAlias(Alias::ALIAS_REPLICA)
-                ->build();
-
-            $mainIndexName = $this->indexNameBuilder
-                ->setIndexId(InventoryIndexer::INDEXER_ID)
-                ->addDimension('stock_', (string)$stockId)
-                ->setAlias(Alias::ALIAS_MAIN)
-                ->build();
-
-            $this->indexStructure->delete($replicaIndexName, ResourceConnection::DEFAULT_CONNECTION);
-            $this->indexStructure->create($replicaIndexName, ResourceConnection::DEFAULT_CONNECTION);
-
-            if (!$this->indexStructure->isExist($mainIndexName, ResourceConnection::DEFAULT_CONNECTION)) {
-                $this->indexStructure->create($mainIndexName, ResourceConnection::DEFAULT_CONNECTION);
-            }
-
-            $this->reservationsIndexTable->createTable($stockId);
-            $this->prepareReservationsIndexData->execute($stockId);
-
-            $this->indexHandler->saveIndex(
-                $replicaIndexName,
-                $this->indexDataProviderByStockId->execute((int)$stockId),
-                ResourceConnection::DEFAULT_CONNECTION
-            );
-            $this->indexTableSwitcher->switch($mainIndexName, ResourceConnection::DEFAULT_CONNECTION);
-            $this->indexStructure->delete($replicaIndexName, ResourceConnection::DEFAULT_CONNECTION);
-
-            $this->reservationsIndexTable->dropTable($stockId);
         }
+
+        if (count($stocksToReindex) > 1) {
+            $userFunctions = [];
+            foreach ($stocksToReindex as $stockId) {
+                $userFunctions[] = function () use ($stockId) {
+                    $this->reindexStock($stockId);
+                };
+            }
+            $this->processManager->execute($userFunctions);
+        } elseif ($stocksToReindex) {
+            $this->reindexStock($stocksToReindex[0]);
+        }
+    }
+
+    /**
+     * Rebuild the index of a single stock through the replica table.
+     *
+     * @param int $stockId
+     * @return void
+     */
+    private function reindexStock(int $stockId): void
+    {
+        $replicaIndexName = $this->indexNameBuilder
+            ->setIndexId(InventoryIndexer::INDEXER_ID)
+            ->addDimension('stock_', (string)$stockId)
+            ->setAlias(Alias::ALIAS_REPLICA)
+            ->build();
+
+        $mainIndexName = $this->indexNameBuilder
+            ->setIndexId(InventoryIndexer::INDEXER_ID)
+            ->addDimension('stock_', (string)$stockId)
+            ->setAlias(Alias::ALIAS_MAIN)
+            ->build();
+
+        $this->indexStructure->delete($replicaIndexName, ResourceConnection::DEFAULT_CONNECTION);
+        $this->indexStructure->create($replicaIndexName, ResourceConnection::DEFAULT_CONNECTION);
+
+        if (!$this->indexStructure->isExist($mainIndexName, ResourceConnection::DEFAULT_CONNECTION)) {
+            $this->indexStructure->create($mainIndexName, ResourceConnection::DEFAULT_CONNECTION);
+        }
+
+        $this->reservationsIndexTable->createTable($stockId);
+        $this->prepareReservationsIndexData->execute($stockId);
+
+        $this->indexHandler->saveIndex(
+            $replicaIndexName,
+            $this->indexDataProviderByStockId->execute($stockId),
+            ResourceConnection::DEFAULT_CONNECTION
+        );
+        $this->indexTableSwitcher->switch($mainIndexName, ResourceConnection::DEFAULT_CONNECTION);
+        $this->indexStructure->delete($replicaIndexName, ResourceConnection::DEFAULT_CONNECTION);
+
+        $this->reservationsIndexTable->dropTable($stockId);
     }
 }
