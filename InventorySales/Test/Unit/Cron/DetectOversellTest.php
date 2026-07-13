0bad585b5e7b5b0fe4de4e450836f7e6a05afacd
Jeanmarcos Juarez <janmarcoj@gmail.com>
2026-07-13 08:28:08 -0400

    feat(inventory-sales): add supply-side oversell detection

---
+++ InventorySales/Test/Unit/Cron/DetectOversellTest.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * Copyright 2026 Adobe
+ * All Rights Reserved.
+ */
+declare(strict_types=1);
+
+namespace Magento\InventorySales\Test\Unit\Cron;
+
+use Magento\InventorySales\Cron\DetectOversell;
+use Magento\InventorySales\Model\ResourceModel\SourceReservation\GetOversoldSourceItems;
+use Magento\InventorySales\Model\SourceReservation\OversellDetectionConfig;
+use Magento\InventorySales\Model\SourceReservation\OversellNotifier;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+use Psr\Log\LoggerInterface;
+
+class DetectOversellTest extends TestCase
+{
+    /**
+     * @var OversellDetectionConfig|MockObject
+     */
+    private $config;
+
+    /**
+     * @var GetOversoldSourceItems|MockObject
+     */
+    private $getOversoldSourceItems;
+
+    /**
+     * @var OversellNotifier|MockObject
+     */
+    private $notifier;
+
+    /**
+     * @var DetectOversell
+     */
+    private $cron;
+
+    protected function setUp(): void
+    {
+        $this->config = $this->createMock(OversellDetectionConfig::class);
+        $this->getOversoldSourceItems = $this->createMock(GetOversoldSourceItems::class);
+        $this->notifier = $this->createMock(OversellNotifier::class);
+        $this->cron = new DetectOversell(
+            $this->config,
+            $this->getOversoldSourceItems,
+            $this->notifier,
+            $this->createMock(LoggerInterface::class)
+        );
+    }
+
+    public function testDoesNothingWhenSweepDisabled(): void
+    {
+        $this->config->method('isSweepEnabled')->willReturn(false);
+        $this->getOversoldSourceItems->expects(self::never())->method('execute');
+        $this->notifier->expects(self::never())->method('notify');
+
+        $this->cron->execute();
+    }
+
+    public function testNotifiesWhenOversoldFound(): void
+    {
+        $this->config->method('isSweepEnabled')->willReturn(true);
+        $rows = [['source_code' => 'src', 'sku' => 'sku-1', 'physical' => 0.0, 'reserved' => -2.0, 'delta' => -2.0]];
+        $this->getOversoldSourceItems->method('execute')->willReturn($rows);
+        $this->notifier->expects(self::once())->method('notify')->with($rows);
+
+        $this->cron->execute();
+    }
+
+    public function testDoesNotNotifyWhenNoneFound(): void
+    {
+        $this->config->method('isSweepEnabled')->willReturn(true);
+        $this->getOversoldSourceItems->method('execute')->willReturn([]);
+        $this->notifier->expects(self::never())->method('notify');
+
+        $this->cron->execute();
+    }
+}
---
+++ InventorySales/Test/Unit/Model/SourceReservation/OversellNotifierTest.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * Copyright 2026 Adobe
+ * All Rights Reserved.
+ */
+declare(strict_types=1);
+
+namespace Magento\InventorySales\Test\Unit\Model\SourceReservation;
+
+use Magento\Framework\Notification\NotifierInterface;
+use Magento\InventorySales\Model\SourceReservation\OversellNotifier;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+use Psr\Log\LoggerInterface;
+
+class OversellNotifierTest extends TestCase
+{
+    /**
+     * @var LoggerInterface|MockObject
+     */
+    private $logger;
+
+    /**
+     * @var NotifierInterface|MockObject
+     */
+    private $notifier;
+
+    /**
+     * @var OversellNotifier
+     */
+    private $oversellNotifier;
+
+    /**
+     * @var int
+     */
+    private $warnings;
+
+    protected function setUp(): void
+    {
+        $this->warnings = 0;
+        $this->logger = $this->createMock(LoggerInterface::class);
+        $this->logger->method('warning')->willReturnCallback(function () {
+            $this->warnings++;
+        });
+        $this->notifier = $this->createMock(NotifierInterface::class);
+        $this->oversellNotifier = new OversellNotifier($this->logger, $this->notifier);
+    }
+
+    public function testLogsEachPositionAndPushesOneAdminNotice(): void
+    {
+        $this->notifier->expects(self::once())->method('addMajor');
+
+        $this->oversellNotifier->notify([
+            $this->item('src-a', 'sku-1', 1.0, -4.0),
+            $this->item('src-b', 'sku-2', 0.0, -2.0),
+        ]);
+
+        self::assertSame(2, $this->warnings);
+    }
+
+    public function testEmptyDoesNothing(): void
+    {
+        $this->notifier->expects(self::never())->method('addMajor');
+
+        $this->oversellNotifier->notify([]);
+
+        self::assertSame(0, $this->warnings);
+    }
+
+    public function testSwallowsAdminNoticeFailure(): void
+    {
+        $this->notifier->method('addMajor')->willThrowException(new \RuntimeException('inbox down'));
+
+        $this->oversellNotifier->notify([$this->item('src-a', 'sku-1', 1.0, -4.0)]);
+
+        self::assertSame(2, $this->warnings);
+    }
+
+    /**
+     * @param string $source
+     * @param string $sku
+     * @param float $physical
+     * @param float $reserved
+     * @return array<string, mixed>
+     */
+    private function item(string $source, string $sku, float $physical, float $reserved): array
+    {
+        return [
+            'source_code' => $source,
+            'sku' => $sku,
+            'physical' => $physical,
+            'reserved' => $reserved,
+            'delta' => $physical + $reserved,
+        ];
+    }
+}
